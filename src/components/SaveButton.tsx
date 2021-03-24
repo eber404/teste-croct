dae98d9841b345e568d857feb7e9e094241d0c3a
eber404 <bodacamargo@gmail.com>
2021-03-23 21:53:53 -0300

    feat: saveButton styled component

---
+++ src/components/SaveButton.tsx
@@ -0,0 +1,27 @@
+import { Button } from 'rebass/styled-components'
+import styled from 'styled-components'
+
+const SaveButton = styled(Button)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 109px;
+  height: 32px;
+  background: #3d485f;
+  border-radius: 16px;
+  margin-top: 35px;
+  margin-left: auto;
+
+  font-family: Inter;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 180%;
+  letter-spacing: -0.02em;
+
+  cursor: pointer;
+
+  color: #ffffff;
+`
+
+export default SaveButton
