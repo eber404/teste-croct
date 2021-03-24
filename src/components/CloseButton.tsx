b2b810c2d8c983c9c76e427ccad5b32338b4f9c7
eber404 <bodacamargo@gmail.com>
2021-03-23 21:53:39 -0300

    feat: closeButton styled component

---
+++ src/components/CloseButton.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Box, BoxProps } from 'reflexbox'
+import styled from 'styled-components'
+import SVGComponent from './SVGComponent'
+
+export interface ICloseButton extends BoxProps {
+  callback?: () => unknown
+}
+
+const CloseButton: React.FC<ICloseButton> = (props: ICloseButton) => {
+  return (
+    <SBox {...props} onClick={() => props.callback()}>
+      <SVGComponent
+        src="/assets/icons/close.svg"
+        marginLeft="auto"
+        marginBottom="auto"
+      />
+    </SBox>
+  )
+}
+
+const SBox = styled(Box)`
+  position: relative;
+  cursor: pointer;
+`
+
+export default CloseButton
